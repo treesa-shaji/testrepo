45a3422852b57b143ee5629558bda5a12ac927e7
Treesa Shaji <treesa.shaji@alphaplus.co.uk>
2023-08-10 16:10:28 +0100

    #connecting R and github

--- git codes.R
+++ git codes.R
@@ -2,3 +2,13 @@ library(usethis)
 
 #getting and storing a PAT.
 create_github_token()
+#token - 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
+#storing PAT to connect R and Github
+library(gitcreds)
+gitcreds_set()
+
+#getting the project on github
+use_git()
+
+#create a GitHub repo and connect it to your current RStudio project
+use_github()
